e8060866ad3819faf3000707eadbf772ffbc0c8d
Joshua Logan <joshlogan32323@gmail.com>
2016-07-11 14:32:15 -0700

    added Yelp Api keys

---
+++ app/src/main/java/com/epicodus/foodcarttracker/Constants.java
@@ -0,0 +1,11 @@
+package com.epicodus.foodcarttracker;
+
+/**
+ * Created by Joshua on 7/11/16.
+ */
+public class Constants {
+    public static final String YELP_CONSUMER_KEY = BuildConfig.YELP_CONSUMER_KEY;
+    public static final String YELP_CONSUMER_SECRET = BuildConfig.YELP_CONSUMER_SECRET;
+    public static final String YELP_TOKEN = BuildConfig.YELP_TOKEN;
+    public static final String YELP_TOKEN_SECRET = BuildConfig.YELP_TOKEN_SECRET;
+}
